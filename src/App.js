b6b0f66ed31a7b968dfef27dbf3be62b911b58e1
Harsh Wandhare <81346147+CodeWithHarsh@users.noreply.github.com>
2023-12-14 00:07:38 +0530

    Revert "Login Screen"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from "./pages/login";
-import LoginSelection from "./pages/loginselection";
-import Register from "./pages/register";
-
+import classes from "./App.module.scss";
 import Dashboard from "./pages/dashboard";
+import MyComponent from "./pages/dashboard";
 function App() {
-  return (
-    <div className="app">
-      <Router>
-        <Routes>
-          <Route path="/" element={<LoginSelection />} />
-          <Route path="/login/student" element={<Login userType="student" />} />
-          <Route exact path="/register" element={<Register />} />
-          <Route exact path="/dashboard" element={<Dashboard />} />
-        </Routes>
-      </Router>
-    </div>
-  );
+    return (
+        <div className={classes.App}>
+            <Dashboard />
+        </div>
+    );
 }
 
 export default App;
